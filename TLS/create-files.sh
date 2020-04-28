d18f205f7f1f056fd481eb97d866837c4436e02a
root <root@cool1.blr.asicdesigners.com>
2020-04-28 17:28:36 +0530

    Edited test file creation script

--- TLS/create-files.sh
+++ TLS/create-files.sh
@@ -1,5 +1,14 @@
-fallocate -l 1K 1K 
-fallocate -l 1M 1M 
-fallocate -l 1G 1G 
-fallocate -l 2G 2G 
+#fallocate -l 1K 1K 
+
+echo "Creating 1K file"
+dd if=/dev/random of=1K bs=1024 count=1 iflag=fullblock status=progress
+#fallocate -l 1M 1M 
+echo "Creating 1M file"
+dd if=/dev/random of=1M bs=1024 count=1024 iflag=fullblock status=progress
+#fallocate -l 1G 1G 
+echo "Creating 1G file"
+dd if=/dev/random of=1G bs=1024 count=1024000 iflag=fullblock status=progress
+#fallocate -l 2G 2G 
+echo "Creating 2G file"
+dd if=/dev/random of=2G bs=1024 count=2048000 iflag=fullblock status=progress
 
